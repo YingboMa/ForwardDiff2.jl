85fea0f1c2753594741ac3698e82dbf45730be00
ShashiGowda <gowda@mit.edu>
2019-08-08 18:27:45 -0400

    more overdubs

--- src/ForwardDiff2.jl
+++ src/ForwardDiff2.jl
@@ -3,6 +3,7 @@ module ForwardDiff2
 export DualArray
 
 include("dualarray.jl")
+include("dual_context.jl")
 # TODO: differentiation API (jacobian...)
 
 end # module
--- src/dual_context.jl
+++ src/dual_context.jl
@@ -1,28 +1,76 @@
 using Cassette
+
+import Cassette: overdub
 using ChainRules
 
 Cassette.@context DualContext
 
-using ForwardDiff: Dual, value, partials, tagtype
-@inline _first_dual(i) = 0
-@inline _first_dual(i, x, tail...) = x isa Dual ? i : _first_dual(i-1, tail...)
+using ForwardDiff: Dual, value, partials, tagtype, ≺
 
-@inline last_dual(xs...) = _first_dual(length(xs), reverse(xs)...)
+@inline _dominant_dual(tag, maxi, i) = maxi
 
-@inline function Cassette.overdub(ctx::DualContext,
-                          f,
-                          args...)
-    idx = last_dual(args...)
+@inline function _dominant_dual(T, maxi, i, x::Dual{S}, tail...) where {S}
+    if T === nothing || (T !== S && T ≺ S)
+        _dominant_dual(S, i, i-1, tail...)
+    else
+        _dominant_dual(T, maxi, i-1, tail...)
+    end
+end
+
+_dominant_dual(tag, maxi, i, x, tail...) = _dominant_dual(tag, maxi, i-1, tail...)
+
+@inline dominant_dual(xs...) = _dominant_dual(nothing, 0, length(xs), reverse(xs)...)
+
+@inline _value(::Val{T}, x) where T = x
+@inline _value(::Val{T}, d::Dual{T}) where T = value(d)
+@inline function _value(::Val{T}, d::Dual{S}) where {T,S}
+    if S ≺ T
+        d
+    else
+        throw(DualMismatchError(T,S))        
+    end
+end
+
+@inline Base.@propagate_inbounds _partials(::Val{T}, x, i...) where T = partials(x, i...)
+@inline Base.@propagate_inbounds _partials(::Val{T}, d::Dual{T}, i...) where T = partials(d, i...)
+@inline function _partials(::Val{T}, d::Dual{S}, i...) where {T,S}
+    if S ≺ T
+        partials(zero(d))
+    else
+        throw(DualMismatchError(T,S))
+    end
+end
+
+@inline function overdub(ctx::DualContext,
+                         f,
+                         args...)
+    idx = dominant_dual(args...)
     if idx === 0
         Cassette.recurse(ctx, f, args...)
     else
-        res = frule(f, map(value, args)...)
+        dtag = tagtype(args[idx])
+        res = overdub(ctx, frule, f,
+                      map(x->_value(Val{dtag}(), x), args)...)
         if res === nothing
             Cassette.recurse(ctx, f, args...)
         else
+          # @show dtag
+          # @show args
+            as = map(x->_value(Val{dtag}(), x), args)
+          # @show as
+            ps = map(x->_partials(Val{dtag}(), x), args)
+          # @show ps
             val, ∂ = res
-            Dual{tagtype(args[idx])}(val, ∂(map(partials, args)...))
+            Dual{dtag}(val,
+                       overdub(ctx,
+                               ∂,
+                               map(x->_partials(Val{dtag}(), x),
+                                   args)...))
         end
     end
 end
 
+function dualrun(f, args...)
+    ctx = DualContext()
+    Cassette.overdub(ctx, f, args...)
+end
