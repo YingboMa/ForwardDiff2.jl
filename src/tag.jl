4122da9ce0824633efb65c3b2bf70222563109f0
ShashiGowda <gowda@mit.edu>
2019-08-15 20:30:58 -0400

    Make find_dual completely generated
    Before:
    
    ```julia
    
    julia> @time D(x -> x * D(y -> x + y, 1), 1)
     41.852295 seconds (127.60 M allocations: 6.823 GiB, 5.71% gc time)
    1
    julia> @time D(x -> x * D(y -> x + y, 1), 1)
      1.962306 seconds (5.69 M allocations: 311.423 MiB, 6.14% gc time)
    1
    ```
    
    after
    
    ```julia
    julia> @time D(x -> x * D(y -> x + y, 1), 1)
     12.301352 seconds (40.46 M allocations: 2.227 GiB, 4.98% gc time)
    1
    
    julia> @time D(x -> x * D(y -> x + y, 1), 1)
      0.467172 seconds (1.62 M allocations: 91.768 MiB, 5.42% gc time)
    1
    ```

--- src/tag.jl
+++ src/tag.jl
@@ -3,18 +3,18 @@ import ForwardDiff: Dual
 struct Tag{Parent} end
 
 _find_dual(ctx::Type{T}, i) where {T} = 0
-_find_dual(ctx::Type{T}, i, x::Dual{T}, xs...) where {T} = i
+_find_dual(ctx::Type{T}, i, x::Type{<:Dual{T}}, xs...) where {T} = i
 _find_dual(ctx::Type{T}, i, x, xs...) where {T} = _find_dual(ctx, i-1, xs...)
 
 innertagtype(::Type{Tag{T}}) where T = T
-Base.@pure @inline function find_dual(::Type{T}, xs...) where {T<:Tag}
+@inline @generated function find_dual(::Type{T}, xs...) where {T<:Tag}
     idx = _find_dual(T, length(xs), reverse(xs)...)
     idx === 0 ?
-        find_dual(innertagtype(T), xs...) : idx
+    _find_dual(innertagtype(T), length(xs), reverse(xs)...) : idx
 end
 
 # Base case where T is not a Tag
-Base.@pure @inline find_dual(T, xs...) = 0
+@inline @generated find_dual(T, xs...) = 0
 
 using Test
 
