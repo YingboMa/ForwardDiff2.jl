310fd1f1b734b6496b872cac741ce943e922fdf9
Shashi Gowda <shashigowda91@gmail.com>
2019-12-27 11:43:32 -0500

    stash

--- src/dual_context.jl
+++ src/dual_context.jl
@@ -3,20 +3,23 @@ using ChainRules
 using ChainRulesCore
 import ChainRulesCore: Wirtinger, Zero
 
-using Cassette: overdub, Context, nametype
+using Cassette: overdub, Context, nametype, similarcontext
 
 Cassette.@context DualContext
 
 const TaggedCtx{T} = Context{nametype(DualContext),T}
 
 function dualcontext()
+    # Note that the `dualtag()` is not of the same type as that of the
+    # Duals constructed in this context, because it is called in the older context
+    # New duals are of type Tag{typeof(dualtag())}
     Cassette.disablehooks(DualContext(metadata=dualtag(), pass=CustomDispatchPass))
 end
 
 # Calls to `dualtag` are aware of the current context. Note that the tags
 # produced in the current context are `Tag{T} where T` is the metadata type of
 # the context.
-@inline Cassette.overdub(ctx::C, ::typeof(dualtag)) where {T,C<:TaggedCtx{T}} = Tag{T}()
+@inline Cassette.overdub(ctx::TaggedCtx{T}, ::typeof(dualtag)) where {T} = Tag{T}()
 
 @inline Cassette.overdub(ctx::TaggedCtx, ::typeof(find_dual), args...) = find_dual(args...)
 
@@ -24,10 +27,8 @@ end
 @inline _value(::Tag{T}, d::Dual{Tag{T}}) where T = value(d)
 
 
-@inline _partials(::Any, x, i...) = Zero()
-@inline _partials(::Dual, x, i...) = partials(x, i...)
-@inline _partials(::Tag{T}, d::Dual{Tag{T}}, i...) where T = partials(d, i...)
-@inline _partials(::Tag{T}, x::Dual{S}, i...) where {T,S} = Zero()
+@inline _partials(::Any, x) = Zero()
+@inline _partials(::Tag{T}, d::Dual{Tag{T}}) where T = d.partials
 
 #=
 function Wirtinger(primal::Partials, conjugate::Union{Number,ChainRulesCore.AbstractDifferential})
@@ -40,8 +41,11 @@ end
 @inline _values(S, xs) = map(x->_value(S, x), xs)
 @inline _partialss(S, xs) = map(x->_partials(S, x), xs)
 
-@inline function _frule_overdub(ctx::TaggedCtx{S}, tag::T, f, args...) where {T,S}
+@inline function _frule_overdub(context::TaggedCtx{S}, f, args...) where {T,S}
+    tag = context.metadata
     vs = _values(tag, args)
+    ctx = similarcontext(context, metadata=innertagtype(tag))
+    #ctx = context
     res = alternative(ctx, frule, f, vs...)
 
     if res === nothing
@@ -93,17 +97,90 @@ end
 @inline isinteresting(ctx::TaggedCtx, f, a, b, c) = anydual(a, b, c)
 @inline isinteresting(ctx::TaggedCtx, f, a, b, c, d) = anydual(a, b, c, d)
 @inline isinteresting(ctx::TaggedCtx, f, args...) = false
+@inline isinteresting(ctx::TaggedCtx, f::typeof(Base.show), args...) = false
+
+function _frule_overdub2(ctx::TaggedCtx{T}, f, args...) where T
+    # Here we can assume that one or more `args` is a Dual with tag
+    # of type T.
+
+    tag = Tag{T}()
+    # unwrap only duals with the tag T.
+    vs = _values(tag, args)
+    @show vs
+
+    # call frule to see if there is a rule for this call:
+    if ctx.metadata isa Tag
+        ctx1 = similarcontext(ctx, metadata=innertag(ctx.metadata))
+
+        # we call frule with an older context because the Dual numbers may
+        # themselves contain Dual numbers that were created in an older context
+        frule_result = overdub(ctx1, frule, f, vs...)
+    else
+        frule_result = frule(f, vs...)
+    end
+
+    if frule_result === nothing
+        # this means there is no frule
+        # We can't just do f(args...) here because `f` might be
+        # a closure which closes over a Dual number, hence we call
+        # recurse. Recurse overdubs the calls inside `f` and not `f` itself
+        return Cassette.recurse(ctx, f, args...)
+    else
+        # this means there exists an frule for this specific call.
+        # frule_result is then a tuple (val, pushforward) where val
+        # is the primal result. (Note: this may be Dual numbers but only
+        # with an older tag)
+        val, pushforward = frule_result
+
+        # extract the partials only for the current tag
+        # so we can pass them to the pushforward
+        ps = _partialss(tag, args)
+
+        # Call the pushforward to get new partials
+        # we call it with the older context because the partials
+        # might themselves be Duals from older contexts
+        ctx1 = similarcontext(ctx, metadata=innertag(ctx.metadata))
+        ∂s = overdub(ctx1, pushforward, Zero(), ps...)
+
+        # Attach the new partials to the primal result
+        # multi-output `f` such as result in the new partials being
+        # a tuple, we handle both cases:
+        return if ∂s isa Tuple
+            map(val, ∂s) do v, ∂
+                Dual{T}(v, ∂)
+            end
+        else
+            Dual{T}(val, ∂s)
+        end
+    end
+end
 
 @inline function alternative(ctx::TaggedCtx{T}, f, args...) where {T}
-    idx = find_dual(Tag{T}(), args...)
+    # This method only executes if `args` contains at least 1 Dual
+    # the question is what is its tag
+
+    tag = Tag{T}()
+
+    idx = find_dual(tag, args...)
     if idx === 0
-        # none of the arguments are dual
-        return overdub(ctx, f, args...)
+        # This is the base case for the recursion in this function which
+        # tries to do the alternative with successively older contexts
+        # the oldest context is not a `Tag`
+        if !(T<:Tag)
+            return Cassette.fallback(ctx, f, args...)
+        end
+        # none of the arguments have the same tag as the context
+        # try with the parent context
+        ctx1 = similarcontext(ctx, metadata=innertag(ctx.metadata))
+        return overdub(ctx1, f, args...)
+    elseif f === Dual
+        overdub(ctx, f, args...)
     else
-        tag = tagtype(fieldtype(typeof(args), idx))()
+        println("Dual found")
+        @show f, args
         # call ChainRules.frule to execute `f` and
         # get a function that computes the partials
-        return _frule_overdub(ctx, tag, f, args...)
+        return _frule_overdub2(ctx, f, args...)
     end
 end
 
--- src/tag.jl
+++ src/tag.jl
@@ -1,19 +1,14 @@
 struct Tag{Parent} end
 
-@inline ≺(::Any, ::Tag) = true
-@inline ≺(::Tag, ::Any) = false
-@inline ≺(::Any, ::Any) = false
-@inline ≺(t::Tag, s::Tag) = innertagtype(t) ≺ innertagtype(s)
-
 @inline _find_dual(tag::T, l, i) where {T} = 0
 @inline _find_dual(tag::T, l, i, x::Dual{T}, xs...) where {T} = i
 @inline _find_dual(tag::T, l, i, x, xs...) where {T} = _find_dual(tag, l, i+1, xs...)
 
-@inline innertagtype(::Tag{Tag{T}}) where T = Tag{T}()
-@inline innertagtype(::Tag{T}) where T = nothing
+@inline innertag(::Tag{Tag{T}}) where T = Tag{T}()
+@inline innertag(::Tag{T}) where T = nothing
+
 @inline function find_dual(T::TT, xs...) where {TT<:Tag}
-    idx = _find_dual(T, length(xs), 1, xs...)
-    idx === 0 ? find_dual(innertagtype(T), xs...) : idx
+    _find_dual(T, length(xs), 1, xs...)
 end
 
 # Base case where T is not a Tag
