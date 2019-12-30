8250f576ae1e21cf9c023e8cde8d6ad8cbb29476
Shashi Gowda <shashigowda91@gmail.com>
2019-12-30 11:45:34 -0500

    Separate value and partials in DualArray, delete rest of the code.
    Co-authored-by: "Yingbo Ma" <mayingbo5@gmail.com>
    Co-authored-by: "Shashi Gowda" <gowda@mit.edu>

--- src/dualarray.jl
+++ src/dualarray.jl
@@ -1,10 +1,18 @@
 using StaticArrays: SVector
+
+partial_type(::Dual{T,V,P}) where {T,V,P} = P
 # TODO: Tagging?
 # TODO: Integrate better with SVector. Maybe even use SIMD.jl?
 
-struct DualArray{T,E,M,D<:AbstractArray,I} <: AbstractArray{E,M}
-    data::D
-    DualArray{T,I}(a::AbstractArray{E,N}) where {T,I,E,N} = new{T,E,N-1,typeof(a),I}(a)
+struct DualArray{T,E,M,V<:AbstractArray,D<:AbstractArray} <: AbstractArray{E,M}
+    data::V
+    partials::D
+    function DualArray{T}(v::AbstractArray{E,N}, p::AbstractArray) where {T,E,N}
+        # TODO: Fix the empty array case
+        M = ndims(p)
+        VT = typeof(@view p[ntuple(_->1, Val(M-1))..., :])
+        return new{T,Dual{T,E,VT},N,typeof(v),typeof(p)}(v, p)
+    end
 end
 
 ###
@@ -18,91 +26,35 @@ function Base.print_array(io::IO, da::DualArray)
     Base.printstyled(io, "Primals:\n", bold=false, color=2)
     prev_params = io isa IOContext ? io.dict : ()
     ioc = IOContext(io, prev_params..., sz)
-    Base.print_array(ioc, value(da))
+    Base.print_array(ioc, data(da))
     Base.println(io)
-    for i=1:npartials(da)
-        Base.printstyled(io,"Partials($i):\n", bold=false, color=3)
-        Base.print_array(ioc, getindex.(partials.(da), i))
-        i !== npartials(da) && Base.println(io)
-    end
+    Base.printstyled(io,"Partials:\n", bold=false, color=3)
+    Base.print_array(ioc, allpartials(da))
     return nothing
 end
 
-DualArray(a::AbstractArray) = DualArray{Nothing,size(a, ndims(a))-1}(a)
-npartials(d::DualArray{T,E,M,D,I}) where {T,E,M,D,I} = I
-tagtype(::Type{<:DualArray{T}}) where {T} = T
-tagtype(::T) where {T<:DualArray} = tagtype(T)
+DualArray(a::AbstractArray, b::AbstractArray) = DualArray{typeof(dualtag())}(a, b)
+npartials(d::DualArray) = size(d.partials, ndims(d.partials))
 data(d::DualArray) = d.data
+allpartials(d::DualArray) = d.partials
 
 ###
 ### Array interface
 ###
 
 Base.eltype(d::DualArray{T,E}) where {T,E} = Dual{T,E,npartials(d)}
-droplast(d::Tuple) = d |> reverse |> Base.tail |> reverse
-Base.size(d::DualArray) = size(data(d)) |> droplast
-Base.size(d::DualArray, i) = i <= ndims(d) ? size(data(d), i) : 1
+#droplast(d::Tuple) = d |> reverse |> Base.tail |> reverse
+Base.size(d::DualArray) = size(data(d))
 Base.IndexStyle(d::DualArray) = Base.IndexStyle(data(d))
-Base.strides(d::DualArray) = strides(data(d)) |> droplast
-Base.similar(d::DualArray{T}, ::Type{S}, dims::Dims) where {T,S} = DualArray{T,npartials(d)}(similar(data(d), S, (dims..., npartials(d)+1)))
+Base.similar(d::DualArray{T}, ::Type{S}, dims::Dims) where {T, S} = DualArray{T}(similar(data(d)), similar(allpartials(d)))
+Base.eachindex(d::DualArray) = eachindex(data(d))
 
-###
-### Broadcast interface
-###
-
-using Base.Broadcast: Broadcasted, BroadcastStyle, AbstractArrayStyle, DefaultArrayStyle
-
-struct DualStyle{M,T,I,D} <: AbstractArrayStyle{M}
+Base.@propagate_inbounds function Base.getindex(d::DualArray{T}, i::Int...) where {T}
+    return Dual{T}(data(d)[i...], @view allpartials(d)[i..., :])
 end
 
-Base.Broadcast.BroadcastStyle(::Type{<:DualArray{T,E,M,D,I}}) where {T,E,M,D,I} = DualStyle{M,T,I,typeof(Base.Broadcast.BroadcastStyle(D))}()
-function Base.similar(bc::Broadcasted{<:DualStyle{M,T,I,D}}, ::Type{E}) where {M,T,I,D,E}
-    if E <: Dual
-        V = valtype(E)
-        arr = DualArray{T,I}(similar(Array{V}, (axes(bc)..., Base.OneTo(I+1)))) # TODO: work with arbitrary array types. Maybe use `ArrayInterface.jl`?
-    else
-        bc′ = convert(Broadcasted{D}, bc)
-        arr = Base.similar(bc′, E)
-    end
-    return arr
-end
-Base.BroadcastStyle(::DualStyle{M,T,I,D}, ::DualStyle{M,T,I,V}) where {M,T,I,D,V} = DualStyle{M,T,I,typeof(Base.BroadcastStyle(D(), V()))}()
-Base.BroadcastStyle(::DualStyle{M,T,I,D}, B::BroadcastStyle) where {M,T,I,D} = DualStyle{M,T,I,typeof(Base.BroadcastStyle(D(), B))}()
-Base.BroadcastStyle(::DualStyle{M,T,I,D}, B::DefaultArrayStyle) where {M,T,I,D} = DualStyle{M,T,I,typeof(Base.BroadcastStyle(D(), B))}()
-
-function value(d::DualArray)
-    n = ndims(d)
-    dd = data(d)
-    return @view dd[ntuple(_ -> Colon(), Val(n))..., 1]
-end
-
-function partials(d::DualArray)
-    n = ndims(d)
-    dd = data(d)
-    return @view dd[ntuple(_ -> Colon(), Val(n))..., 2:end]
-end
-
-Base.eachindex(d::DualArray) = eachindex(@view data(d)[:, 1])
-
-Base.@propagate_inbounds function Base.getindex(d::DualArray, i::Int...)
-    dd = data(d)
-    # TODO: do something different if dd is not Linear index style
-    ii = LinearIndices(size(d))[i...]
-    val   = dd[ii]
-    slice_len = length(d)
-    parts = ntuple(j->dd[j * slice_len + ii], Val(npartials(d)))
-    return Dual(val, SVector(parts))
-end
-
-Base.@propagate_inbounds function Base.setindex!(d::DualArray, dual, i::Int...)
-    dd = data(d)
-    ii = LinearIndices(size(d))[i...]
-    dd[ii] = value(dual)
-
-    slice_len = length(d)
-    ps = partials(dual)
-    for j = 1:npartials(d)
-        dd[j * slice_len + ii] = ps[j]
-    end
+Base.@propagate_inbounds function Base.setindex!(d::DualArray{T}, dual::Dual{T}, i::Int...) where {T}
+    data(d)[i...] = value(dual)
+    allpartials(d)[i..., :] .= partials(dual)
     return dual
 end
