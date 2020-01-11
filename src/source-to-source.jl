f350d8ffb874deafe70fae72c4aa02a8f1488bd3
Yingbo Ma <mayingbo5@gmail.com>
2020-01-10 23:20:27 -0500

    Update the template

--- src/source-to-source.jl
+++ src/source-to-source.jl
@@ -1,16 +1,23 @@
-f(x) = sin(cos(x))
+# Template
+g(x) = tan(x) * 2x + 10x
+f(x) = sin(cos(x)) * g(x)
 
-f(x)
-y, d = applyfrule(f, x, d)
-y, d = applyfrule(f, x, d)
-y, d = applyfrule(f, x, d)
-y, d = applyfrule(f, x, d)
-function f'(x, Δ)
-    x1 = cos(x)
-    dx1 = -Δ*sin(x)
-    x2 = sin(x1)
-    dx2 = dx1*cos(x2)
-    return x2, dx2
+function dg(Δ, x)
+    dself = Zero()
+    x1, Δ1 = frule(dself, tan, x, Δ)
+    x2, Δ2 = frule(dself, *, 2, x, Zero(), Δ)
+    x3, Δ3 = frule(dself, *, x1, x2, Δ1, Δ2)
+    x4, Δ4 = frule(dself, *, 10, x, Zero(), Δ)
+    x5, Δ5 = frule(dself, +, x3, x4, Δ3, Δ4)
+    return x5, Δ5
 end
 
-f'(Δx) = (-cos(cos(x)) * sin(x)) * Δx
+function df(Δ, x)
+    dself = Zero()
+    x1, Δ1 = frule(dself, cos, x, Δ)
+    x2, Δ2 = frule(dself, sin, x1, Δ1)
+    x3, Δ3 = frule(dself, tan, x2, Δ2)
+    x4, Δ4 = dg(x, x3, Δ3)
+    x5, Δ5 = frule(dself, *, x3, x4, Δ3, Δ4)
+    return x5, Δ5
+end
