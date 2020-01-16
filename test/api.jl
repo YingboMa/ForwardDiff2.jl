43b5b89155e57abd88726f02cb78425912c521aa
Yingbo Ma <mayingbo5@gmail.com>
2020-01-15 22:38:58 -0500

    Update tests

--- test/api.jl
+++ test/api.jl
@@ -5,26 +5,32 @@ using OrdinaryDiffEq, ForwardDiff
 using LinearAlgebra: norm
 
 @testset "Derivative, Gradient, Jacobian, and Hessian" begin
-    @test D(sin)(1.0) === cos(1.0)
-    @test D(float)(1) === 1
-    @test D(AbstractFloat)(1) === 1
+    @test D(sin, 1) * true === cos(1.0)
+    @test D(sin, 1) * I === cos(1.0) * I
+    @test D(float, 1) * 1 === 1
+    @test D(AbstractFloat, 1) * 1 === 1
+    @test D(x->[x, x^2], 3) * 1 == [1, 6]
     # Gradient
-    @test D(x->[x, x^2])(3) == [1, 6]
-    @test D(sum)([1,2,3]) == ones(3)'
+    @test D(sum, [1,2,3]) * I == ones(3)'
     # Jacobian
-    @test D(x->@SVector([x[1]^x[2], x[3]^3, x[3]*x[2]*x[1]]))(@SVector[1,2,3.]) === @SMatrix [2.0 0 0; 0 0 27; 6 3 2]
-    @test D(cumsum)(@SVector([1,2,3])) == @SMatrix [1 0 0; 1 1 0; 1 1 1]
-    @test D(cumsum)([1,2,3]) == [1 0 0; 1 1 0; 1 1 1]
-    @test D(x->@SVector([x[1], x[2]]))(@SVector([1,2,3])) === @SMatrix [1 0 0; 0 1 0]
+    @test D(x->@SVector([x[1]^x[2], x[3]^3, x[3]*x[2]*x[1]]), @SVector[1,2,3.]) * I === @SMatrix [2.0 0 0; 0 0 27; 6 3 2]
+    @test D(cumsum, @SVector([1,2,3])) * I == @SMatrix [1 0 0; 1 1 0; 1 1 1]
+    @test D(cumsum, [1,2,3]) * I == [1 0 0; 1 1 0; 1 1 1]
+    @test D(x->@SVector([x[1], x[2]]), @SVector([1,2,3])) * I === @SMatrix [1 0 0; 0 1 0]
     # Hessian
-    @test D(D(x->x[1]^x[2] + x[3]^3 + x[3]*x[2]*x[1]))(@SVector[1,2,3]) === @SMatrix [2 4 2; 4 0 1; 2 1 18.]
-    @test D(D(x->x[1]^x[2] + x[3]^3 + x[3]*x[2]*x[1]))([1,2,3]) == [2 4 2; 4 0 1; 2 1 18.]
+    @test D(x -> D(x->x[1]^x[2] + x[3]^3 + x[3]*x[2]*x[1], x) * I, @SVector[1,2,3]) * I === @SMatrix [2 4 2; 4 0 1; 2 1 18.]
+    @test D(x -> D(x->x[1]^x[2] + x[3]^3 + x[3]*x[2]*x[1], x) * I, [1,2,3]) * I == [2 4 2; 4 0 1; 2 1 18.]
 end
 
 @testset "Inference" begin
-    @inferred D(x->exp(x) + x^x + cos(x) + tan(x) + 2^x)(1)
+    simple_D = @inferred D(x->exp(x) + x^x + cos(x) + tan(x) + 2^x, 1)
+    @inferred simple_D * 1
+    @inferred simple_D * true
+    @inferred simple_D * I
+
+    complicated_D = D(x->exp(x) + x^x + cos(x) + tan(x) + 2^x + log(cos(x)) + sec(pi*x) - angle(x) + one(x) / log1p(sin(x)), 1)
     # broken due to `Core._apply`
-    @test_broken @inferred D(x->exp(x) + x^x + cos(x) + tan(x) + 2^x + log(cos(x)) + sec(pi*x) - angle(x) + one(x) / log1p(sin(x)))(1)
+    @test_broken @inferred complicated_D * 1
 end
 
 @testset "Differentiate an ODE solver" begin
@@ -45,8 +51,12 @@ end
 
     u0 = [1.0, 0.0, 0.0]
     for tf in [1, 10, 100.0]
-        fd2 = D(x->odesolver(x, ForwardDiff2.value, tf))(u0)
+        fd2 = D(x->odesolver(x, ForwardDiff2.value, tf), u0) * I
         fd = ForwardDiff.jacobian(x->odesolver(x, ForwardDiff.value, tf), u0)
         @test fd2 ≈ fd rtol=1e-12 # Wow, chaos!
     end
+end
+
+@testset "No perturbation confusion" begin
+    @test D(x -> x * D(y -> x + y, 1), 1) * 1 === 1
 end
